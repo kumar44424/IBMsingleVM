67a1d4ba6edbb8133c4e0a0855ab13b9d6bb3c92
kumar44424 <45359851+kumar44424@users.noreply.github.com>
2020-01-08 11:46:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,9 +114,9 @@ resource "ibm_compute_ssh_key" "temp_public_key" {
   network_speed            = 10
   hourly_billing           = true
   private_network_only     = false
-  cores                    = 1
-  memory                   = 1024
-  disks                    = [25]
+  cores                    = 2
+  memory                   = 2048
+  disks                    = [100]
   dedicated_acct_host_only = false
   local_disk               = false
   tags                     = ["${module.camtags.tagslist}"]
